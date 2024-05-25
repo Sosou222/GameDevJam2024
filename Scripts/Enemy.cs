2ddda6f19b2e2ef2258b4a4f427da6d249f38801
Sosou222 <szym.baran222@gmail.com>
2024-05-25 20:41:10 +0200

    added ability to detect in wchich direction enmey is moving
    snaped to four direction movemnt

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -6,6 +6,8 @@ public partial class Enemy : PathFollow2D
 
 	public float Speed = 50.0f;
 
+	private Vector2 direction = Vector2.Right;
+
 	public override void _Ready()
 	{
 	}
@@ -13,6 +15,24 @@ public partial class Enemy : PathFollow2D
 
 	public override void _Process(double delta)
 	{
+		Vector2 oldPos = GlobalPosition;
+
 		Progress += Speed * (float)delta;
+
+		direction = SnapVector(oldPos);
+		GD.Print($"Direction:" + direction);
+	}
+
+	private Vector2 SnapVector(Vector2 oldPosition)
+	{
+		Vector2 dir = (GlobalPosition - oldPosition).Normalized();
+		if (Mathf.Abs(dir.X) > Mathf.Abs(dir.Y))
+		{
+			return new Vector2(Mathf.Sign(dir.X), 0);
+		}
+		else
+		{
+			return new Vector2(0, Mathf.Sign(dir.Y));
+		}
 	}
 }
