df60469b0c5c763053bcdd3bd6f8caa1b6d99bfa
Sosou222 <szym.baran222@gmail.com>
2024-05-29 23:24:45 +0200

    saving some progress

--- Scripts/Tower.cs
+++ Scripts/Tower.cs
@@ -8,44 +8,39 @@ public partial class Tower : Node2D
 
 	[Export] private PackedScene bulletScene;
 
-	private Dictionary<int, Node2D> weaponsHolders = new();
-	private Node2D currentWeaponHolder;
-	private Marker2D currentMarker;
-	private AnimatedSprite2D currentSprites;
-
-
-	private int currentLevel = 1;
-
-	private AtlasTexture towerBaseAtlasTexture;
-
-
+	private Node2D weaponHolder;
+	private Marker2D shootMarker;
 	private EnemyDetectionComponent enemyDetectionComponent;
 
 	private List<Enemy> enemies => enemyDetectionComponent.enemies;
 
-
-	private Timer timer;
+	private Tween shootTween;
 
 	private const int pixelAtlasSeperationX = 64;
 
 	public override void _Ready()
 	{
-		towerBaseAtlasTexture = GetNode<Sprite2D>("TowerBaseSprite").Texture as AtlasTexture;
+		weaponHolder = GetNode<Node2D>("WeaponHolder");
+		shootMarker = weaponHolder.GetNode<Marker2D>("Marker2D");
 		enemyDetectionComponent = GetNode<EnemyDetectionComponent>("EnemyDetectionComponent");
-		timer = GetNode<Timer>("Timer");
-
-		timer.Timeout += Shoot;
-
-		weaponsHolders.Add(1, GetNode<Node2D>("Weapons/LV1"));
-		weaponsHolders.Add(2, GetNode<Node2D>("Weapons/LV2"));
-		weaponsHolders.Add(3, GetNode<Node2D>("Weapons/LV3"));
-
-		SetLevel(2);
 	}
 
 	public override void _Process(double delta)
 	{
+		if (shootTween == null)
+		{
 
+		}
+	}
+
+	private void TryShooting()
+	{
+		enemies.RemoveAll(e => e == null || !IsInstanceValid(e));
+		Enemy en = EnemiesManager.GetFirstEnemyInSightOrNull(enemies);
+		if (en != null)
+		{
+
+		}
 	}
 
 	private void Shoot()
@@ -55,38 +50,16 @@ public partial class Tower : Node2D
 		if (en != null)
 		{
 			//GD.Print($"Enemy {en.Name} progres:{en.ProgressRatio}");
-			currentWeaponHolder.LookAt(en.GlobalPosition);
-			Vector2 dirToEnemy = (en.GlobalPosition - currentMarker.GlobalPosition).Normalized();
+			weaponHolder.LookAt(en.GlobalPosition);
+			Vector2 dirToEnemy = (en.GlobalPosition - shootMarker.GlobalPosition).Normalized();
 
 			Bullet bullet = bulletScene.Instantiate<Bullet>();
 			bullet.Init(dirToEnemy);
-			bullet.GlobalPosition = currentMarker.GlobalPosition;
+			bullet.GlobalPosition = shootMarker.GlobalPosition;
 			bullet.LookAt(en.GlobalPosition);
 
 			GetTree().Root.AddChild(bullet);
 		}
-	}
-
-	private void SetLevel(int level)
-	{
-		currentLevel = level;
-
-		Rect2 tmpReg = towerBaseAtlasTexture.Region;
-		towerBaseAtlasTexture.Region = new Rect2(new Vector2(tmpReg.Size.X * (currentLevel - 1), tmpReg.Position.Y), tmpReg.Size);
-		SetCurrentGolders(currentLevel);
-	}
-
-	private void SetCurrentGolders(int level)
-	{
-		foreach (var weapon in weaponsHolders)
-		{
-			weapon.Value.Visible = false;
-		}
-		currentWeaponHolder = weaponsHolders[level];
-		currentMarker = currentWeaponHolder.GetNode<Marker2D>("Marker2D");
-		currentSprites = currentWeaponHolder.GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-
-		currentWeaponHolder.Visible = true;
 	}
 
 }
