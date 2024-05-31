e4e7626732626a3d35daa0560ae26d585c191ca8
Sosou222 <szym.baran222@gmail.com>
2024-05-31 02:32:02 +0200

    added code opetion to detroy on arrival bullet

--- Scripts/Components/MovementComponents/ToTargetMovementComponent.cs
+++ Scripts/Components/MovementComponents/ToTargetMovementComponent.cs
@@ -5,6 +5,7 @@ public partial class ToTargetMovementComponent : IMovementComponent
 {
     [Export] private Node2D owner;
     [Export] private float Speed = 300.0f;
+    [Export] private bool DestroyOnArrival = true;
     private Vector2 targetPosition;
     public override void Init(Vector2 targetPos)
     {
@@ -14,5 +15,9 @@ public partial class ToTargetMovementComponent : IMovementComponent
     public override void _Process(double delta)
     {
         owner.GlobalPosition = owner.GlobalPosition.MoveToward(targetPosition, Speed * (float)delta);
+        if (!DestroyOnArrival)
+            return;
+        if (owner.GlobalPosition == targetPosition)
+            owner.QueueFree();
     }
 }
