45e04898ec1041284b091280f096c0c0c3d49f70
Sosou222 <szym.baran222@gmail.com>
2024-06-02 20:22:43 +0200

    added ability to deselect by cliclking enywhere or other buttons

--- Scripts/Components/TowerSelectionComponent.cs
+++ Scripts/Components/TowerSelectionComponent.cs
@@ -12,6 +12,18 @@ public partial class TowerSelectionComponent : Area2D
 
 	}
 
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton button)
+		{
+			if (button.ButtonIndex == MouseButton.Left && !button.Pressed)
+			{
+				return;
+			}
+			GameManager.Instance.SetTargetTower(null);
+		}
+	}
+
 	private void OnInputEvent(Node viewport, InputEvent inputEvent, long shapeIDX)
 	{
 		if (inputEvent is InputEventMouseButton button)
