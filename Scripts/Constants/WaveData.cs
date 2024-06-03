e6f076830d5a8b3b0b8a9ee17700be9f7474c5de
Sosou222 <szym.baran222@gmail.com>
2024-06-03 21:17:12 +0200

    updated wave data

--- Scripts/Constants/WaveData.cs
+++ Scripts/Constants/WaveData.cs
@@ -23,75 +23,73 @@ public class WaveData
     {
         { 0,  new Array<WaveInfo>
         {
-            new WaveInfo(EnemyType.FireBug,3),
-            new WaveInfo(EnemyType.Leafbug,5)
+            new WaveInfo(EnemyType.Leafbug,20),
         }},
         { 1, new Array<WaveInfo>
         {
+            new WaveInfo(EnemyType.Leafbug,20),
             new WaveInfo(EnemyType.FireBug,5),
-            new WaveInfo(EnemyType.Leafbug,7)
         }},
         { 2, new Array<WaveInfo>
         {
+            new WaveInfo(EnemyType.FireBug,4),
             new WaveInfo(EnemyType.Leafbug,10),
-            new WaveInfo(EnemyType.FireBug,5),
-            new WaveInfo(EnemyType.Leafbug,3),
+            new WaveInfo(EnemyType.FireBug,4),
+            new WaveInfo(EnemyType.Leafbug,10),
         }}
         ,
         { 3, new Array<WaveInfo>
         {
-            new WaveInfo(EnemyType.Leafbug,10),
+            new WaveInfo(EnemyType.Leafbug,15),
             new WaveInfo(EnemyType.FireBug,5),
-            new WaveInfo(EnemyType.Leafbug,3),
+            new WaveInfo(EnemyType.Leafbug,15),
         }}
         ,
         { 4, new Array<WaveInfo>
         {
-            new WaveInfo(EnemyType.Leafbug,10),
-            new WaveInfo(EnemyType.FireBug,5),
-            new WaveInfo(EnemyType.Leafbug,3),
+            new WaveInfo(EnemyType.Leafbug,5),
+            new WaveInfo(EnemyType.FireBug,10),
+            new WaveInfo(EnemyType.Leafbug,5),
+            new WaveInfo(EnemyType.FireBug,20),
         }}
         ,
         { 5, new Array<WaveInfo>
         {
-            new WaveInfo(EnemyType.Leafbug,10),
-            new WaveInfo(EnemyType.FireBug,5),
-            new WaveInfo(EnemyType.Leafbug,3),
+            new WaveInfo(EnemyType.FireBug,45),
         }}
         ,
         { 6, new Array<WaveInfo>
         {
-            new WaveInfo(EnemyType.Leafbug,10),
-            new WaveInfo(EnemyType.FireBug,5),
-            new WaveInfo(EnemyType.Leafbug,3),
+            new WaveInfo(EnemyType.Leafbug,20),
+            new WaveInfo(EnemyType.FireBug,15),
+            new WaveInfo(EnemyType.Leafbug,20),
         }}
         ,
         { 7, new Array<WaveInfo>
         {
-            new WaveInfo(EnemyType.Leafbug,10),
-            new WaveInfo(EnemyType.FireBug,5),
-            new WaveInfo(EnemyType.Leafbug,3),
+            new WaveInfo(EnemyType.FireBug,20),
+            new WaveInfo(EnemyType.Leafbug,30),
+            new WaveInfo(EnemyType.FireBug,15),
         }}
         ,
         { 8, new Array<WaveInfo>
         {
-            new WaveInfo(EnemyType.Leafbug,10),
-            new WaveInfo(EnemyType.FireBug,5),
-            new WaveInfo(EnemyType.Leafbug,3),
+            new WaveInfo(EnemyType.Leafbug,30),
+            new WaveInfo(EnemyType.FireBug,15),
+            new WaveInfo(EnemyType.Leafbug,30),
         }}
         ,
         { 9, new Array<WaveInfo>
         {
-            new WaveInfo(EnemyType.Leafbug,10),
-            new WaveInfo(EnemyType.FireBug,5),
-            new WaveInfo(EnemyType.Leafbug,3),
+            new WaveInfo(EnemyType.Leafbug,40),
+            new WaveInfo(EnemyType.FireBug,30),
+            new WaveInfo(EnemyType.Leafbug,15),
         }}
         ,
         { 10, new Array<WaveInfo>
         {
-            new WaveInfo(EnemyType.Leafbug,10),
-            new WaveInfo(EnemyType.FireBug,5),
-            new WaveInfo(EnemyType.Leafbug,3),
+            new WaveInfo(EnemyType.FireBug,50),
+            new WaveInfo(EnemyType.Leafbug,60),
         }}
     };
 }
